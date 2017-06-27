d1e903a0a2fbb52c22f805ba4d4535043b859b2c
Federico Sismondi <federicosismondi@gmail.com>
2017-06-27 15:34:05 +0200

    pep8

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ import os
 import base64
 
 
-def save_pcap_from_base64(filename, pcap_file_base64, dir = None):
+def save_pcap_from_base64(filename, pcap_file_base64, dir=None):
     """
     Returns number of bytes saved.
 
@@ -14,10 +14,10 @@ def save_pcap_from_base64(filename, pcap_file_base64, dir = None):
 
     if dir:
         file_path = os.path.join(dir, filename)
-    else :
+    else:
         file_path = os.path.join(os.getcwd(), filename)
 
-    with open( file_path, "wb") as pcap_file:
+    with open(file_path, "wb") as pcap_file:
         nb = pcap_file.write(base64.b64decode(pcap_file_base64))
         return nb
 
@@ -31,9 +31,8 @@ def get_from_environment(variable, default):
         logging.warning("Using default variable %s=%s" % (variable, default))
     return v
 
+
 if __name__ == '__main__':
-
     # example of use for save_pcap_from_base64 function
-    base_64_pcap_value='1MOyoQIABAAAAAAAAAAAAMgAAABlAAAASJQxWTqkAwA5AAAAOQAAAGAOcR8AERFAu7sAAAAAAAAAAAAAAAAAAbu7AAAAAAAAAAAAAAAAAALk2RYzABGuhFAEANS0dGVzdA=='
-    save_pcap_from_base64('test.pcap',base_64_pcap_value)
-
+    base_64_pcap_value = '1MOyoQIABAAAAAAAAAAAAMgAAABlAAAASJQxWTqkAwA5AAAAOQAAAGAOcR8AERFAu7sAAAAAAAAAAAAAAAAAAbu7AAAAAAAAAAAAAAAAAALk2RYzABGuhFAEANS0dGVzdA=='
+    save_pcap_from_base64('test.pcap', base_64_pcap_value)
